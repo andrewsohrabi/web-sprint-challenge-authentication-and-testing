09709babc316a9187dda15c197a29ac22676f481
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-26 04:19:48 -0700

    explicitly added select * to users-model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,20 +1,20 @@
 const db = require("../../data/dbConfig");
 
 function find() {
-    return db('users');
+    return db('users').select('*');
 }
 
 function findBy(filter) {
-    return db('users').where(filter);
+    return db('users').select('*').where(filter);
 }
 
 async function add(user) {
-    const [id] = await db('users').insert(user,'id')
+    const [id] = await db('users').insert(user,'id').select('*')
     return findById(id);
 }
 
 function findById(id) {
-    return db('users').where('id',id).first();
+    return db('users').select('*').where('id',id).first();
 }
 
 module.exports = {
